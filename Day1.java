163072aaaeda0f6923dbd34f3e31a6b95bc745aa
Jaskaran Kukreja <35613378+Jaskaran121@users.noreply.github.com>
2019-02-16 01:03:17 -0500

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -2,6 +2,9 @@ import java.awt.List;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+//Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+//For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 public class Day1 {
 	public Boolean calculate(int[] array,int k) {
 		for(int i=0;i<array.length;i++)
