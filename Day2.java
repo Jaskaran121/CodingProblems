3e1f4e08b39187a28e3e6f96a2d52cf00d97bfe8
Jaskaran Kukreja <35613378+Jaskaran121@users.noreply.github.com>
2019-02-18 14:57:48 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,5 +1,10 @@
 import java.util.*;
 import java.util.stream.Collectors;
+
+//Given an array of integers, find the first missing positive integer in linear time and constant
+//space. In other words, find the lowest positive integer that does not exist in the array. The
+//array can contain duplicates and negative numbers as well.
+
 public class Day2 {
 	public int calculate(int[] array)
 	{
